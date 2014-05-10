cf19d1d8c3787efaa301757332a819be205547d2
Matt Rubin <opensource@mattrubin.me>
2014-05-10 19:09:14 -0400

    Stub MSQWebViewController

--- Masquerade/Source/MSQAppDelegate.m
+++ Masquerade/Source/MSQAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "MSQAppDelegate.h"
+#import "MSQWebViewController.h"
 
 
 @implementation MSQAppDelegate
@@ -15,6 +16,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+
+    self.window.rootViewController = [MSQWebViewController new];
+
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ Masquerade/Source/MSQWebViewController.h
@@ -0,0 +1,14 @@
+//
+//  MSQWebViewController.h
+//  Masquerade
+//
+//  Created by Matt Rubin on 5/10/14.
+//  Copyright (c) 2014 Matt Rubin. All rights reserved.
+//
+
+@import UIKit;
+
+
+@interface MSQWebViewController : UIViewController
+
+@end
